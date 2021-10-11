f3749775396978b4bfe49dbb3b635f892ef7d242
sanjaydraws <sprajapat8331@gmail.com>
2021-10-11 23:17:49 +0530

    added product flavors

--- app/src/main/java/com/sanjay/progaurdexample/MainActivity.kt
+++ app/src/main/java/com/sanjay/progaurdexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.sanjay.progaurdexample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import com.sanjay.progaurdexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         binding?.apply {
-            setContentView(R.layout.activity_main)
+            setContentView(this.root)
         }
 
         callService(BuildConfig.BASE_URL)
@@ -30,11 +31,11 @@ class MainActivity : AppCompatActivity() {
             binding?.option5?.visibility = View.GONE
         }else if(url =="http://www.paidapp.com"){
 
-            binding?.option1?.visibility = View.VISIBLE
-            binding?.option2?.visibility = View.VISIBLE
-            binding?.option3?.visibility = View.VISIBLE
-            binding?.option4?.visibility = View.VISIBLE
-            binding?.option5?.visibility = View.VISIBLE
+            binding?.option1?.isVisible = true
+            binding?.option2?.isVisible = true
+            binding?.option3?.isVisible = true
+            binding?.option4?.isVisible = true
+            binding?.option5?.isVisible = true
         }
     }
 }
